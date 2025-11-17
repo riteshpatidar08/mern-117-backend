62859e2174e37420e15eea99d10a5a255eac74c6
ritesh <riteshpatidar088@gmail.com>
2025-11-17 12:26:46 +0530

    updated till path module

--- index.js
+++ index.js
@@ -1,104 +1,152 @@
-// //client and server
+// // //client and server
 
-// //client => html , css and js
+// // //client => html , css and js
 
-// //client => js
-// //server => nodejs
+// // //client => js
+// // //server => nodejs
 
-// //client => html + js
-// //server => js
-// //browser => node
+// // //client => html + js
+// // //server => js
+// // //browser => node
 
-// // node index.js  //server //runtime environment
-// //client html + js
+// // // node index.js  //server //runtime environment
+// // //client html + js
 
-// //NOTE client
-// // dom manipulation
-// //client => jab user interaction karega tab event occur hoga => click , form submit , mouse over
+// // //NOTE client
+// // // dom manipulation
+// // //client => jab user interaction karega tab event occur hoga => click , form submit , mouse over
 
-// //NOTE server ?
-// //note file data read
-// //note server 24*7
-// //events imp
+// // //NOTE server ?
+// // //note file data read
+// // //note server 24*7
+// // //events imp
 
-// //modules =>
+// // //modules =>
 
-// //client side js vs serverside js difference ?
-// //dom manipulation vs file handling , server
-// //event interaction based vs  events
+// // //client side js vs serverside js difference ?
+// // //dom manipulation vs file handling , server
+// // //event interaction based vs  events
 
-// //window object kya wo node main available hoga ?
-// // console.log(global)
-// // console.log(window)
+// // //window object kya wo node main available hoga ?
+// // // console.log(global)
+// // // console.log(window)
 
-// //es6 module
+// // //es6 module
 
-// //package.json  npm init
+// // //package.json  npm init
 
-// const figlet = require('figlet');
-// const { firstname, age } = require('./script.js');
-// // const  {course }= require('./new.js');
+// // const figlet = require('figlet');
+// // const { firstname, age } = require('./script.js');
+// // // const  {course }= require('./new.js');
 
-// figlet(require('./new.js'), function (err, data) {
-//   if (err) {
-//     console.log('Something went wrong...');
-//     console.dir(err);
-//     return;
-//   }
-//   console.log(data);
-// });
+// // figlet(require('./new.js'), function (err, data) {
+// //   if (err) {
+// //     console.log('Something went wrong...');
+// //     console.dir(err);
+// //     return;
+// //   }
+// //   console.log(data);
+// // });
 
-// //common js module //es6 module
-// //cjs  //esm
-// console.log('hello')
+// // //common js module //es6 module
+// // //cjs  //esm
+// // console.log('hello')
 
 
-// //dependencies ==> npm i/install express
-// //devDendencies => npm install -D nodemon 
-// //devDendencies => npm install nodemon --save-dev
-// //NOTE package installation
-// //nodemon ? locally => npm install nodemon ,
-// //  globally  npm install -g nodemon
+// // //dependencies ==> npm i/install express
+// // //devDendencies => npm install -D nodemon 
+// // //devDendencies => npm install nodemon --save-dev
+// // //NOTE package installation
+// // //nodemon ? locally => npm install nodemon ,
+// // //  globally  npm install -g nodemon
 
-// // express :  5.1.2 => semantic versioning => major.minor.patch
-// //patch => 5.1.3
-// //minor => 5.2.3
-// //major => 6.2.3
+// // // express :  5.1.2 => semantic versioning => major.minor.patch
+// // //patch => 5.1.3
+// // //minor => 5.2.3
+// // //major => 6.2.3
 
-//NOTE FILE SYSTEM 
+// //NOTE FILE SYSTEM 
 
+// const fs = require('fs') ;
+
+// //two types of functions available in this module 
+
+// //Synchronous fn 
+// console.log('read file started...')
+// const data  = fs.readFileSync('./index.txt' , 'utf-8') ;
+//  console.log(data)
+//  console.log('read file ended...')
+
+// //asynchronous fn
+// console.log('async read file started')
+// fs.readFile('./index.txt', 'utf-8',(err,data)=>{
+//   console.log(data)
+// })
+// console.log('async file ended') ;
+
+// //NOTE write file 
+
+// // fs.writeFileSync('newfile.txt' , 'hello this is a write file') ;
+
+// // const student = [ {name : "ritesh"} , {name :"devesh"}] ; //json
+// // //json.stringify => used to convert array of object into json
+// // fs.writeFileSync('studentdata.json' , JSON.stringify(student))
+
+
+// //suppose i have one 
+
+// // fs.readFile('./post.json' , 'utf-8' , (err,data)=>{
+// //   // if(err) console.log(err)
+// //     console.log(data)
+// //   fs.writeFileSync('newpost.json' , data)
+// // })
+
+// const postdata = fs.readFileSync('./post.json' , 'utf-8') ;
+// fs.writeFileSync('writepost.txt', postdata) ;
+
+
+// //rename file
+// fs.rename('writepost.txt' , 'writepost.json', (err)=>{
+// console.log('rename success')
+// })
+
+// //unlink (delete file)
+// fs.unlink('writepost.json', (err)=>{
+// console.log('delete success')
+// })
+
+// //new folder 
+// fs.mkdir('dashboard', (err)=>{
+
+// })
+
+// fs.rmdir('dashboard' , ()=>{
+
+// })
+
+
+const path = require('path') ;
 const fs = require('fs') ;
 
-//two types of functions available in this module 
+const joinedpath = path.join('dashboard' , 'users' , 'users.js') ;
+console.log(joinedpath)
 
-//Synchronous fn 
-console.log('read file started...')
-const data  = fs.readFileSync('./index.txt' , 'utf-8') ;
- console.log(data)
- console.log('read file ended...')
+const filepath = path.join('dashboard','index.txt') ;
+console.log(filepath) ;
 
-//asynchronous fn
-console.log('async read file started')
-fs.readFile('./index.txt', 'utf-8',(err,data)=>{
-  console.log(data)
+fs.readFile(filepath, 'utf-8', (err,data)=>{
+console.log(data)
 })
-console.log('async file ended') ;
+// ./index.js
+//path module => file path findout
 
-//NOTE write file 
+//finding absolute path using path.join (__filename , __dirname) ;
+console.log('filename' ,__filename , 'dirname', __dirname)
 
-// fs.writeFileSync('newfile.txt' , 'hello this is a write file') ;
+//NOTE use this pattern to find out any path of file inside your project
+const abpath = path.join(__dirname , 'dashboard', 'index.txt') ;
+console.log(abpath)
 
-// const student = [ {name : "ritesh"} , {name :"devesh"}] ; //json
-// //json.stringify => used to convert array of object into json
-// fs.writeFileSync('studentdata.json' , JSON.stringify(student))
-
-
-//suppose i have one 
-
-// fs.readFile('./post.json' , 'utf-8' , (err,data)=>{
-//   // if(err) console.log(err)
-//     console.log(data)
-//   fs.writeFileSync('newpost.json' , data)
-// })
-const postdata = fs.readFileSync('./post.json' , 'utf-8') ;
-fs.writeFileSync('writepost.txt', postdata) ;
+fs.readFile(abpath, 'utf-8', (err,data)=>{
+  console.log(data)
+  })
